eb6a1fae7fa54da08859c6fae97e95ca4fc2b6d7
subinsong01 <haely97@naver.com>
2024-01-09 14:49:02 +0900

    css까지 완성

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,55 +2,14 @@ import React, { useState } from 'react';
 import Square from './Square';
 import './Board.css';
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xnext, setXnext] = useState(true);
-
-  const whoIsWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
-    ];
-
-    for (let index = 0; index < lines.length; index++) {
-      const [a, b, c] = lines[index];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-
-    return null;
-  };
-
-  const winner = whoIsWinner(squares);
-
-  const status = winner ? `Winner: ${winner}` : `Next player: ${xnext ? 'X' : 'O'}`;
-
-  const handleClick = (i) => {
-    const newSquares = squares.slice();
-
-    if (whoIsWinner(newSquares) || newSquares[i]) {
-      return;
-    }
-
-    newSquares[i] = xnext ? 'X' : 'O';
-    setSquares(newSquares);
-    setXnext((previousState) => !previousState);
-  };
+const Board = ({squares, onClick}) => {
 
   const renderSquare = (i) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
   };
 
   return (
-    <div>
-      <div className='status'>{status} </div>
+    <div className='board-wrapper'>
       <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
